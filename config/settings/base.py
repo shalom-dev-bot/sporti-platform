f9fba76484f8fc8c8ccc90d929e2a59026bdef09
SPORTI Dev Team <dev@sporti.app>
2026-07-16 00:32:04 +0100

    fix: ajout de daphne dans INSTALLED_APPS pour activer Channels/ASGI

--- config/settings/base.py
+++ config/settings/base.py
@@ -21,6 +21,7 @@ DEBUG = env.bool("DJANGO_DEBUG", default=False)
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

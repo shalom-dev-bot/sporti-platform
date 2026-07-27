4af310ba6dd930e612f4c6d5cfd8992575c7ef43
SPORTI Dev Team <dev@sporti.app>
2026-07-27 23:40:08 +0100

    Ajout creation/reset superuser automatique au build (demo)

--- build.sh
+++ build.sh
@@ -9,3 +9,20 @@ npm run build-css
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+if username and password:
+    user, created = User.objects.get_or_create(username=username, defaults={'email': email})
+    user.email = email
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(password)
+    user.save()
+    print('Superuser', username, 'pret (cree ou mis a jour)')
+"
